f3945637ee73e677cfe74aee58410dec4f2195ee
Stevan Little <stevan.little@iinteractive.com>
2013-10-22 20:57:46 -0400

    making sure that an empty body will work

--- httpapp_test.go
+++ httpapp_test.go
@@ -14,16 +14,23 @@ func (app *TestApp) ServeHTTP (w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *TestApp) Call (e *Env) *Response {
-    resp := NewResponse(200)
-    resp.Header.Add("Content-Length", "7")
-    resp.Body.Write([]byte("HELLO WORLD"))
-    return resp
+    path := e.Request.URL.Path
+    switch path {
+        case "/simple":
+            resp := NewResponse(200)
+            resp.Header.Add("Content-Length", "7")
+            resp.Body.Write([]byte("HELLO WORLD"))
+            return resp
+        case "/empty":
+            return NewResponse(204)
+    }
+    return nil
 }
 
 func TestSimpleApp (t *testing.T) {
     app := new(TestApp)
 
-    req, err := http.NewRequest("GET", "http://example.com/", nil)
+    req, err := http.NewRequest("GET", "http://example.com/simple", nil)
     if err != nil {
         t.Logf("got an error: %s", err)
         t.FailNow()
@@ -49,4 +56,27 @@ func TestSimpleApp (t *testing.T) {
         )
     }
 }
+
+func TestEmptyApp (t *testing.T) {
+    app := new(TestApp)
+
+    req, err := http.NewRequest("GET", "http://example.com/empty", nil)
+    if err != nil {
+        t.Logf("got an error: %s", err)
+        t.FailNow()
+    }
+
+    w := httptest.NewRecorder()
+    app.ServeHTTP(w, req)
+
+
+    if w.Code != 204 {
+        t.Errorf("got wrong status - got: %v expected: %v", w.Code, 204)
+    }
+
+    if w.Body.String() != "" {
+        t.Errorf("expected empty body - got: (%v)", w.Body.String())
+    }
+}
+
 
