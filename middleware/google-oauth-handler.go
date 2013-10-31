3f7c061308d7b82224fec2f404e1ed74bca30133
Stevan Little <stevan.little@iinteractive.com>
2013-10-31 16:10:39 -0400

    refactor the google oauth stuff

--- middleware/google-oauth-handler.go
+++ middleware/google-oauth-handler.go
@@ -9,18 +9,11 @@ import (
     "encoding/json"
 
     "github.com/stevan/httpapp"
+    "github.com/stevan/httpapp/middleware/auth"
     "github.com/stevan/httpapp/middleware/sessions"
 
     "code.google.com/p/goauth2/oauth"
 )
-
-// Simple user objects ...
-
-type GoogleUserInfo struct {
-    Id       string `json:"id"`
-    Email    string `json:"email"`
-    Verified bool   `json:"verified_email"`
-}
 
 // ... Auth middleware
 
@@ -61,35 +54,70 @@ func (a *GoogleOAuthHandler) handleOAuth2Callback (e *httpapp.Env) *httpapp.Resp
     t := &oauth.Transport{ Config: a.OauthConfig }
     t.Exchange(e.Request.FormValue("code"))
 
-    req, err := t.Client().Get("https://www.googleapis.com/oauth2/v1/userinfo")
-    if err != nil { panic(err) }
-    defer req.Body.Close()
-
-    body, _ := ioutil.ReadAll(req.Body)
-
-    //log.Println(string(body))
-
-    var user_info GoogleUserInfo
-    json_err := json.Unmarshal(body, &user_info)
-    if json_err != nil { panic(json_err) }
-
-    if user_info.Email == "" {
-        panic(errors.New(fmt.Sprintf("Incorrect JSON response from Google: %s", body)))
-    }
-
-    //log.Printf("%v", user_info)
+    base_user_info := a.getBaseUserInfo(t)
+    ext_user_info  := a.getExtendedUserInfo(t, base_user_info)
+    user_info      := auth.NewGoogleUser(
+        base_user_info.Id,
+        base_user_info.Email,
+        ext_user_info.FullName,
+    )
 
     session := e.Get("session").(*sessions.Session)
-    session.Data["user"] = &user_info
+    session.Data["user"] = user_info
 
     resp := httpapp.NewResponse(http.StatusFound)
     resp.Headers.Add("Location", session.Data["return_to"].(string))
     return resp
 }
 
+// structs for JSON UnMarshal
 
+type googleUserInfoJSON struct {
+    Id       string `json:"id"`
+    Email    string `json:"email"`
+    Verified bool   `json:"verified_email"`
+}
 
+type googlePeopleInfoJSON struct {
+    Id       string `json:"id"`
+    FullName string `json:"displayName"`
+    Verified bool   `json:"verified"`
+}
 
+func (a *GoogleOAuthHandler) getBaseUserInfo (t *oauth.Transport) *googleUserInfoJSON {
+    req, err := t.Client().Get("https://www.googleapis.com/oauth2/v1/userinfo")
+    if err != nil { panic(err) }
+    defer req.Body.Close()
 
+    body, _ := ioutil.ReadAll(req.Body)
+
+    var user_info googleUserInfoJSON
+    json_err := json.Unmarshal(body, &user_info)
+    if json_err != nil { panic(json_err) }
+
+    if user_info.Id == "" {
+        panic(errors.New(fmt.Sprintf("Incorrect JSON response from Google: %s", body)))
+    }
+
+    return &user_info
+}
+
+func (a *GoogleOAuthHandler) getExtendedUserInfo (t *oauth.Transport, info *googleUserInfoJSON) *googlePeopleInfoJSON {
+    req, err := t.Client().Get(fmt.Sprintf("https://www.googleapis.com/plus/v1/people/%s", info.Id))
+    if err != nil { panic(err) }
+    defer req.Body.Close()
+
+    body, _ := ioutil.ReadAll(req.Body)
+
+    var user_info googlePeopleInfoJSON
+    json_err := json.Unmarshal(body, &user_info)
+    if json_err != nil { panic(json_err) }
+
+    if user_info.Id != info.Id {
+        panic(errors.New(fmt.Sprintf("Incorrect JSON response from Google: %s", body)))
+    }
+
+    return &user_info
+}
 
 
